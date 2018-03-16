9b3fd8126162ce22f56bd4491294a9ca2b9986db
Nathan Wallace <nathan@turbot.com>
2018-03-16 15:02:34 -0400

    Add basic tests

---
+++ test/index.js
@@ -0,0 +1,39 @@
+const _ = require("lodash");
+const assert = require("chai").assert;
+const responses = require("../index.js");
+
+describe("turbot-responses", function() {
+  describe("Format", function() {
+    for (k in responses) {
+      (k => {
+        describe(k, function() {
+          it("has camelcase format for key", function() {
+            assert.strictEqual(k, _.camelCase(k));
+          });
+          it("has code and message fields only", function() {
+            assert.hasAllKeys(responses[k], ["code", "message"]);
+          });
+          it("has integer code", function() {
+            assert.isFinite(responses[k].code);
+          });
+          it("has short string message < 100 chars long", function() {
+            assert.isString(responses[k].message);
+            assert.isAbove(responses[k].message.length, 0);
+            assert.isBelow(responses[k].message.length, 100);
+          });
+        });
+      })(k);
+    }
+  });
+
+  describe("Specific type checks", function() {
+    describe("ok", function() {
+      it("has message OK", function() {
+        assert.equal(responses.ok.message, "OK");
+      });
+      it("has code 200", function() {
+        assert.equal(responses.ok.code, 200);
+      });
+    });
+  });
+});
